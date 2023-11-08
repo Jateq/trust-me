5a915b47fa21d35041959e43b50e36c76a7da344
jateq <temirlan.eraly1@gmail.com>
2023-11-08 23:21:48 +0600

    simple nav-bar component

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-tabs class="nav-bar" v-model="tab">
+        <q-tab name="logo" label="logo" />
+        <q-tab name="TrustPlaces" label="TrustPlaces" />
+        <q-tab name="TrustPeople" label="TrustPeople" />
+        <q-tab name="TrustCompany" label="TrustCompany" />
+        <q-tab name="TrustCoin" label="TrustCoin" />
+        <q-tab name="TrustBusiness" label="TrustBusiness" />
+        <q-tab name="TrustContract" label="TrustContract" />
+        <q-tab name="More" label="More" />
+        <q-tab name="number" label="+77000700455" />
+        <q-tab name="Ru" label="Ru" />
+      </q-tabs>
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default {
+  name: 'MainLayout',
+  components: {
+  },
+  data() {
+    return {
+      tab: "TrustPlaces"
+    };
+  }
+}
+</script>
